0b8711f7c11d626ad331a93ce3ea2072173db688
Jarrod Taylor <email@jarrodtaylor.me>
2025-02-15 10:06:32 -0500

    Adds CLI interface

--- Sources/RESTlessCLI/RESTlessCLI.swift
+++ Sources/RESTlessCLI/RESTlessCLI.swift
@@ -4,9 +4,26 @@ import RESTless
 
 @main
 struct RESTlessCLI: ParsableCommand {
-  static let configuration = CommandConfiguration(commandName: "restless", version: "1.0.0")
+  static let configuration = CommandConfiguration(
+    commandName: "restless", version: "1.0.0")
+
+  @Argument(
+    help: "Relative path of the directory to serve.",
+    completion: .directory,
+    transform: {URL(string: $0, relativeTo: URL.currentDirectory())!})
+  var path: URL
+
+  @Option(
+    name: .shortAndLong,
+    help: "Port for the localhost server.")
+  var port: UInt16 = 8080
+
+  func validate() throws {
+    guard FileManager.default.fileExists(atPath: path.path())
+    else {throw ValidationError("'\(path.path())' does not exist")}
+  }
 
   mutating func run() {
-    print("Hello, world!")
+    print(path, port)
   }
 }
